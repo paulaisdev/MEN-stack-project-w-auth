e99c1445e7e631abae1e4f6870a73b98c458ad72
Paula Allemand <paulaalemand@gmail.com>
2022-03-15 12:50:15 -0300

    update files and folders

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const controller = require("../controller/userController")
+const controller = require("../controllers/userController")
 
 router.get("/all", controller.getAll)
 router.post("/create", controller.createUser)
