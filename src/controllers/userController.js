1bcd273f5e16d2c0e0f3e5d1787a3fabeed8347b
Paula <41296983+paulaallemand@users.noreply.github.com>
2022-12-07 18:56:22 -0300

    Update userControllers.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,7 @@
 const UserSchema = require("../models/userSchema");
 
 const getAll = async (req, res) => {
-  UserSchema.find(function (err, users) {
-    if(err) {
-      res.status(500).send({ message: err.message })
-    }
-      res.status(200).send(users)
-  }) 
+
 };
 
 module.exports = {
