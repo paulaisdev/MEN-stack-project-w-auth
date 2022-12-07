c6d1ca67c8cf859ddfe18b102bcd65bed216958c
Paula <41296983+paulaallemand@users.noreply.github.com>
2022-12-07 18:59:02 -0300

    added comments in user controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,8 @@
 const UserSchema = require("../models/userSchema");
 
 const getAll = async (req, res) => {
-
+  // seu código aqui
+  console.log("Bateu na rota do get all");
 };
 
 module.exports = {
