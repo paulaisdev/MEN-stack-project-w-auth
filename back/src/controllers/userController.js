fcf738a4f650ee683056898c47b0c1cc83f72052
paulaallemand <paulaisdev@gmail.com>
2023-01-19 22:15:02 -0300

    updated moongose methods to updated and delete user

--- back/src/controllers/userController.js
+++ back/src/controllers/userController.js
@@ -1,24 +1,24 @@
-const UserSchema = require("../models/userSchema")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
+const UserSchema = require("../models/userSchema");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const SECRET = process.env.SECRET
+const SECRET = process.env.SECRET;
 
 const getAll = async (req, res) => {
-    try {
-        const allUsers = await UserSchema.find()
-        res.status(200).json(allUsers)
-    } catch (error) {
-        res.status(500).json({
-            message: error.message,
-        })
-    }
-}
+  try {
+    const allUsers = await UserSchema.find();
+    res.status(200).json(allUsers);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 // const getAll = async (req, res) => {
 //     const authHeader = req.get('authorization')
 //     const token = authHeader.split(' ')[1];
-  
+
 //     if (!token) {
 //       return res.status(401).send("Erro no header")
 //     }
@@ -28,77 +28,65 @@ const getAll = async (req, res) => {
 //             return res.status(401).send("Não autorizado")
 //         }
 //     })
-      
+
 //     UserSchema.find(function (err, users) {
 //       if(err) {
 //         res.status(500).send({ message: err.message })
 //       }
 //         res.status(200).send(users)
-//     }) 
+//     })
 //   }
 
 const createUser = async (req, res) => {
-    console.log("CHEGOU REQUISIÇÃO")
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
-    req.body.password = hashedPassword
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
 
-    try {
-        const newUser = new UserSchema(req.body)
+  try {
+    const newUser = new UserSchema(req.body);
 
-        const savedUser = await newUser.save()
+    const savedUser = await newUser.save();
 
-        res.status(200).json({
-            message: "User adicionado com sucesso!",
-            savedUser
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
-    }
-}
+    res.status(200).json({
+      message: "User adicionado com sucesso!",
+      savedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 const updateUserById = async (req, res) => {
-    try {
-        const findUser = await UserSchema.findById(req.params.id)
+  try {
+    const savedUser = await UserSchema.findByIdAndUpdate(req.params.id);
 
-        if (findUser) {            
-            findUser.name = req.body.name || findUser.name
-            findUser.email = req.body.email || findUser.email
-        }
-
-        const savedUser = await findUser.save()
-
-        res.status(200).json({
-            message: "Usuário atualizada com sucesso!",
-            savedUser
-        })
-
-    } catch (error) {
-        console.error(error)
-    }
-}
+    res.status(200).json({
+      message: "User atualizada com sucesso!",
+      savedUser,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const deleteUserById = async (req, res) => {
-    try {
-        const userFound = await UserSchema.findById(req.params.id)
+  try {
+    const userFound = await UserSchema.findByIdAndDelete(req.params.id);
 
-       await userFound.delete()
-
-       res.status(200).json({
-           mensagem: `Usuário '${userFound.email}' deletada com sucesso!`
-       })
-
-    } catch (err) {
-        res.status(400).json({
-            mensagem: err.message
-        })
-    }
-} 
+    res.status(200).json({
+      mensagem: "User deletada com sucesso!",
+    });
+  } catch (err) {
+    res.status(400).json({
+      mensagem: err.message,
+    });
+  }
+};
 
 module.exports = {
-    getAll,
-    createUser,
-    updateUserById, 
-    deleteUserById
-}
+  getAll,
+  createUser,
+  updateUserById,
+  deleteUserById,
+};
