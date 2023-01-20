47b8d38942398dc72ec6921dfbc11c71ab46251a
paulaallemand <paulaisdev@gmail.com>
2023-01-19 22:09:24 -0300

    updated moongose methos to update and delete user by id :eyes:

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,92 +1,81 @@
-const UserSchema = require("../models/userSchema")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
+const UserSchema = require("../models/userSchema");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const SECRET = process.env.SECRET
+const SECRET = process.env.SECRET;
 
 const getAll = async (req, res) => {
-    const authHeader = req.get('authorization')
-    const token = authHeader.split(' ')[1];
-  
-    if (!token) {
-      return res.status(401).send("Erro no header")
-    }
+  const authHeader = req.get("authorization");
+  const token = authHeader.split(" ")[1];
 
-    jwt.verify(token, SECRET, (err) => {
-        if(err) {
-            return res.status(401).send("Não autorizado")
-        }
-    })
-      
-    UserSchema.find(function (err, users) {
-      if(err) {
-        res.status(500).send({ message: err.message })
-      }
-        res.status(200).send(users)
-    }) 
+  if (!token) {
+    return res.status(401).send("Erro no header");
   }
 
-const createUser = async (req, res) => {
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
-    req.body.password = hashedPassword
-
-    try {
-        const newUser = new UserSchema(req.body)
-
-        const savedUser = await newUser.save()
-
-        res.status(200).json({
-            message: "User adicionado com sucesso!",
-            savedUser
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
+  jwt.verify(token, SECRET, (err) => {
+    if (err) {
+      return res.status(401).send("Não autorizado");
     }
-}
+  });
+
+  UserSchema.find(function (err, users) {
+    if (err) {
+      res.status(500).send({ message: err.message });
+    }
+    res.status(200).send(users);
+  });
+};
+
+const createUser = async (req, res) => {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
+
+  try {
+    const newUser = new UserSchema(req.body);
+
+    const savedUser = await newUser.save();
+
+    res.status(200).json({
+      message: "User adicionado com sucesso!",
+      savedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 const updateUserById = async (req, res) => {
-    try {
-        const findUser = await UserSchema.findById(req.params.id)
+  try {
+    const updatedUser = await UserSchema.findByIdAndUpdate(req.params.id);
 
-        if (findUser) {            
-            findUser.name = req.body.name || findUser.name
-            findUser.email = req.body.email || findUser.email
-        }
-
-        const savedUser = await findUser.save()
-
-        res.status(200).json({
-            message: "Usuário atualizada com sucesso!",
-            savedUser
-        })
-
-    } catch (error) {
-        console.error(error)
-    }
-}
+    res.status(200).json({
+      message: "User atualizado com sucesso!",
+      updatedUser,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const deleteUserById = async (req, res) => {
-    try {
-        const userFound = await UserSchema.findById(req.params.id)
+  try {
+    await UserSchema.findByIdAndDelete(req.params.id);
 
-       await userFound.delete()
-
-       res.status(200).json({
-           mensagem: `Usuário '${userFound.email}' deletada com sucesso!`
-       })
-
-    } catch (err) {
-        res.status(400).json({
-            mensagem: err.message
-        })
-    }
-} 
+    res.status(200).json({
+      mensagem: `User deletado com sucesso!`,
+    });
+  } catch (err) {
+    res.status(400).json({
+      mensagem: err.message,
+    });
+  }
+};
 
 module.exports = {
-    getAll,
-    createUser,
-    updateUserById, 
-    deleteUserById
-}
+  getAll,
+  createUser,
+  updateUserById,
+  deleteUserById,
+};
