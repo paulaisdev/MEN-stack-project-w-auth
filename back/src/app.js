03930860697f5ea146741a010c42fc2cb3410829
paulaallemand <paulaisdev@gmail.com>
2023-01-19 22:15:11 -0300

    fixed lint

--- back/src/app.js
+++ back/src/app.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
 
-require('dotenv-safe').config();
+require("dotenv-safe").config();
 
-const db = require('./config/database')
-const userRoutes = require('./routes/userRoutes')
+const db = require("./config/database");
+db.connect();
 
+app.use(express.json());
 
-db.connect() 
+const userRoutes = require("./routes/userRoutes");
+app.use("/users", userRoutes);
 
-app.use(cors())
-app.use(express.json())
-app.use("/users", userRoutes)
-
-module.exports = app
+module.exports = app;
