68cbd0f7ea464c261d19d28a33bd1323f3c58482
paulaallemand <paulaisdev@gmail.com>
2023-01-19 22:16:23 -0300

    lint

--- front/script.js
+++ front/script.js
@@ -1,52 +1,52 @@
 const init = () => {
-    document.getElementById('button-send').addEventListener('click', submitForm);
-}
+  document.getElementById("button-send").addEventListener("click", submitForm);
+};
 
 const submitForm = (e) => {
-    e.preventDefault(); 
+  e.preventDefault();
 
-    const data = accessData()
-    const url = "http://localhost:3000/users"
+  const data = accessData();
+  const url = "http://localhost:3000/users";
 
-    if(!data) {
-        return
-    }
+  if (!data) {
+    return;
+  }
 
-    fetch(`${url}/create`, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json"
-        },
-        credentials: "same-origin"
-      })
-        .then((response) => {
-            return alert("Cadastro realizado com sucesso!")
-        })
-        .catch((e) => {
-            return console.error(e)
-        })
-}
+  fetch(`${url}/create`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "same-origin",
+  })
+    .then((response) => {
+      return alert("Cadastro realizado com sucesso!");
+    })
+    .catch((e) => {
+      return console.error(e);
+    });
+};
 
 const accessData = () => {
-    return { 
-        "name": document.getElementById("input-first").value,
-        "password": document.getElementById("input-password").value,
-        "email": document.getElementById("input-email").value
-    }
-}
+  return {
+    name: document.getElementById("input-first").value,
+    password: document.getElementById("input-password").value,
+    email: document.getElementById("input-email").value,
+  };
+};
 
-init()
+init();
 
 // EXEMPLO DE GET ALL
 // const getAll =  () => {
-//      fetch("http://localhost:3000/users/all")        
+//      fetch("http://localhost:3000/users/all")
 //         .then((response) => {
-//             return response.json()        
+//             return response.json()
 //             .then((data) => {
 //                 return console.log(data)
 //             })
-//         })        
+//         })
 //         .catch((e) => {
 //             return console.error(e)
 //         })
