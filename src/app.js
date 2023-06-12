29ec73a7f4817899f3c44295a86c4397d1ebb1d2
paula allemand <41296983+paulaisdev@users.noreply.github.com>
2023-06-12 19:23:52 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use(cors());
 
 app.use(express.json());
 
-import db from "./config/database";
+import db from "./config/database.js";
 db.connect();
 
 import userRoutes from "./routes/userRoutes";
